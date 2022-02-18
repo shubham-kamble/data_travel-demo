67a9f2effadc9da2adca9c92202f26caa4cafb2a
shubham-kamble <shubham3sept@gmail.com>
2022-02-18 13:41:31 +0530

    initial commit - added images,places.js

---
+++ Places/places.js
@@ -0,0 +1,36 @@
+const places = [
+    {
+      id: '1',
+      name: 'Lago di Braies, Braies',
+      location: 'Italy',
+      image: require('../assets/location1.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.`,
+      ratings:'5.0',
+    },
+    {
+      id: '2',
+      name: 'Siargao island',
+      location: 'Philippines',
+      image: require('../assets/location2.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.`,
+      ratings:'4.7',
+    },
+    {
+      id: '3',
+      name: 'Manarola',
+      location: 'Italy',
+      image: require('../assets/location3.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.`,
+      ratings:'4.5',
+    },
+    {
+      id: '4',
+      name: 'Perhentian Islands',
+      location: 'Malaysia',
+      image: require('../assets/location4.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.`,
+      ratings:'4.9',
+    },
+  ];
+  
+  export default places;
