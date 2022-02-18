d2b9e2d78faa81c05ecd159a75335bc6a1e707b3
shubham-kamble <shubham3sept@gmail.com>
2022-02-18 13:55:18 +0530

    places.js updated

--- Places/places.js
+++ Places/places.js
@@ -1,4 +1,3 @@
-const places = [
     {
       id: '1',
       name: 'Lago di Braies, Braies',
@@ -30,7 +29,4 @@ const places = [
       image: require('../assets/location4.jpg'),
       details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.`,
       ratings:'4.9',
-    },
-  ];
-  
-  export default places;
+    }
