825e63508b935d32861f3060d1b905619b21429e
shubham-kamble <shubham3sept@gmail.com>
2022-02-18 14:36:21 +0530

    places.js updated

--- Places/places.js
+++ Places/places.js
@@ -5,7 +5,7 @@
       "location": "Italy",
       "image": "https://raw.githubusercontent.com/shubham-kamble/data_travel-demo/main/Images/location1.jpg",
       "details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.",
-      "ratings":"5.0",
+      "ratings":"5.0"
     },
     {
       "id": "2",
@@ -13,7 +13,7 @@
       "location": "Philippines",
       "image": "https://raw.githubusercontent.com/shubham-kamble/data_travel-demo/main/Images/location2.jpg",
       "details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.",
-      "ratings":"4.7",
+      "ratings":"4.7"
     },
     {
       "id": "3",
@@ -21,7 +21,7 @@
       "location": "Italy",
       "image": "https://raw.githubusercontent.com/shubham-kamble/data_travel-demo/main/Images/location3.jpg",
       "details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.",
-      "ratings":"4.5",
+      "ratings":"4.5"
     },
     {
       "id": "4",
@@ -29,6 +29,6 @@
       "location": "Malaysia",
       "image": "https://raw.githubusercontent.com/shubham-kamble/data_travel-demo/main/Images/location4.jpg",
       "details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit.",
-      "ratings":"4.9",
+      "ratings":"4.9"
     }
 ]
